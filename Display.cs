fd1f90ca8615b29de9e277a0f3fb841b35bd46f4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-10 10:33:34 +0100

    added line numbers

---
+++ Display.cs
@@ -0,0 +1,20 @@
+﻿namespace terminal;
+
+internal class Display
+{
+    public static void GreyText(string text) 
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.Write(text);    
+
+        Console.ResetColor();
+    }
+
+    public static void WhiteText(string text)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(text);
+
+        Console.ResetColor();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,31 +4,35 @@ internal class Program
 {
     static void Main()
     {
+        Console.CursorVisible = false;
         Console.ForegroundColor = ConsoleColor.White;
+
+        int lineIndex = 1;
 
         while (true)
         {
             Console.Clear();
-            int activeIndex = 0;
 
             for (int i = 1; i < 20; i++)
             {
-                Console.SetCursorPosition(0, i - 1);
-                Console.Write(i);
+                Console.SetCursorPosition(i.ToString().Length == 1 ? 1 : 0, i - 1);
+                Display.GreyText(i.ToString());
 
-                if (activeIndex == i)
-                    Console.Write(" > ");
-
-                string? commandInput = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(commandInput))
-                    continue;
-
-                var commandArgs = commandInput.Split(' ');
-                var command = commandArgs[0];
-
-                Console.WriteLine(command);
+                if (lineIndex == i)
+                    Display.WhiteText(" >  ");
             }
+
+            Console.SetCursorPosition(5, lineIndex - 1);
+            string? commandInput = Console.ReadLine();
+            lineIndex++;
+
+            if (string.IsNullOrWhiteSpace(commandInput))
+                continue;
+
+            var commandArgs = commandInput.Split(' ');
+            var command = commandArgs[0];
+
+            Console.WriteLine(command);
         }
     }
 }
