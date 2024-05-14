c9b0573b368793ecf44c9cedc04122db7eb6d25b
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-14 19:53:28 +0100

    gameloop

--- ConsoleEngine.cs
+++ ConsoleEngine.cs
@@ -1,78 +1,95 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using tgm.Types;
-using static System.Formats.Asn1.AsnWriter;
 
-namespace tgm;
-
-public abstract class ConsoleEngine
+namespace tgm
 {
-    public static List<Sprite2D> Sprites = new();
-
-    public static bool HideCursor { get; set; }
-
-    public static bool HasGravity { get; set; }
-
-    public static Scene2D? ActiveScene { get; set; }
-
-    // physics object
-
-    public static void Start()
+    public abstract class ConsoleEngine
     {
-        if (HideCursor)
-            Console.CursorVisible = false;
+        private static Thread? gameLoopThread;
 
-        if (ActiveScene is null) 
-            throw new NullReferenceException("An active scene is required to run the engine.");
+        public static List<Sprite2D> Sprites = new();
+        public static bool HideCursor { get; set; }
+        public static bool HasGravity { get; set; }
+        public static Scene2D? ActiveScene { get; set; }
 
-        try 
-        { 
-            while (true) 
+        public static void Start()
+        {
+            if (HideCursor)
+                Console.CursorVisible = false;
+
+            if (ActiveScene is null)
+                throw new NullReferenceException("An active scene is required to run the engine.");
+
+            if (gameLoopThread != null && gameLoopThread.IsAlive)
+                throw new InvalidOperationException("The game loop is already running.");
+
+            gameLoopThread = new Thread(GameLoop);
+            gameLoopThread.Start();
+        }
+
+        private static void GameLoop()
+        {
+            try
             {
-                Render();
+                bool exitRequested = false;
 
-                var key = Console.ReadKey();
-                Player? player = Sprites.FirstOrDefault(e => e.GetType() == typeof(Player)) as Player;
-                player!.Move(key);
+                while (!exitRequested)
+                {
+                    Render();
+
+                    if (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(true);
+                        Player? player = Sprites.FirstOrDefault(e => e.GetType() == typeof(Player)) as Player;
+                        player!.Move(key);
+
+                        if (key.Key == ConsoleKey.Escape)
+                            exitRequested = true;
+                    }
+                }
             }
-        }
-        catch (Exception) 
-        {
-            throw;
-        }
-    }
-
-    public static void Render() 
-    {
-        foreach (var sprite in Sprites)
-        {
-            if (sprite.Scene == ActiveScene)
+            catch (Exception)
             {
-                Console.ForegroundColor = sprite.Color;
-
-                Console.SetCursorPosition(sprite.Position.X, sprite.Position.Y);
-                Console.Write(sprite.Character);
-
-                Console.ResetColor();
-            }
-        }
-    }
-
-    public static void RegisterSprite(Sprite2D sprite) 
-    {
-        foreach (var registeredSprite in sprite.Scene.Sprites) 
-        {
-            if (registeredSprite.Position.X == sprite.Position.X &&
-                registeredSprite.Position.Y == sprite.Position.Y && registeredSprite != sprite) 
-            {
-                throw new Exception("A sprite at this vector position has already been registered");
+                throw;
             }
         }
 
-        Sprites.Add(sprite);
-    }
+        public static void Render()
+        {
+            foreach (var sprite in Sprites)
+            {
+                if (sprite.Scene == ActiveScene)
+                {
+                    Console.ForegroundColor = sprite.Color;
 
-    public static void UnregisterSprite(Sprite2D sprite) 
-    {
-        Sprites.Remove(sprite);
+                    Console.SetCursorPosition(sprite.Position.X, sprite.Position.Y);
+                    Console.Write(sprite.Character);
+
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public static void RegisterSprite(Sprite2D sprite)
+        {
+            foreach (var registeredSprite in sprite.Scene.Sprites)
+            {
+                if (registeredSprite.Position.X == sprite.Position.X &&
+                    registeredSprite.Position.Y == sprite.Position.Y && registeredSprite != sprite)
+                {
+                    throw new Exception("A sprite at this vector position has already been registered");
+                }
+            }
+
+            Sprites.Add(sprite);
+        }
+
+        public static void UnregisterSprite(Sprite2D sprite)
+        {
+            Sprites.Remove(sprite);
+        }
     }
 }
