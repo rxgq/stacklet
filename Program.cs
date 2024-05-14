beddb2dd39145003bcf8e5ba3c8c8471b35b576c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-14 20:36:13 +0100

    line algorithm

--- Program.cs
+++ Program.cs
@@ -10,12 +10,45 @@ internal class Program
         Scene2D grasslandsScene = new("Grasslands");
 
         ConsoleEngine.RegisterSprite(new Player(Vector2.Zero(), grasslandsScene, 'P', ConsoleColor.White));
-        ConsoleEngine.RegisterShape(new Block(new Vector2(10, 10), grasslandsScene, 'P', ConsoleColor.White));
+
+        RegisterManyShapes(startPosition: new Vector2(3, 3), endPosition: new Vector2(10, 20), grasslandsScene);
 
         ConsoleEngine.HideCursor = true;
         ConsoleEngine.HasGravity = false;
         ConsoleEngine.ActiveScene = grasslandsScene;
 
         ConsoleEngine.Start();
+    }
+
+    public static void RegisterManyShapes(Vector2 startPosition, Vector2 endPosition, Scene2D scene)
+    {
+        int dx = Math.Abs(endPosition.X - startPosition.X);
+        int dy = Math.Abs(endPosition.Y - startPosition.Y);
+        int sx = startPosition.X < endPosition.X ? 1 : -1;
+        int sy = startPosition.Y < endPosition.Y ? 1 : -1;
+        int err = dx - dy;
+
+        int x = startPosition.X;
+        int y = startPosition.Y;
+
+        while (true)
+        {
+            ConsoleEngine.RegisterShape(new Block(new Vector2(x, y), scene, '0', ConsoleColor.DarkGray));
+
+            if (x == endPosition.X && y == endPosition.Y)
+                break;
+
+            int e2 = 2 * err;
+            if (e2 > -dy)
+            {
+                err -= dy;
+                x += sx;
+            }
+            if (e2 < dx)
+            {
+                err += dx;
+                y += sy;
+            }
+        }
     }
 }
