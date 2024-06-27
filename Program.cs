7e2b3a855506a988bae8c4c61a99d8bcccbbba21
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-06-27 13:54:26 +0100

    re

--- Program.cs
+++ Program.cs
@@ -1,24 +1,9 @@
-﻿namespace tgm;
+﻿namespace assembly;
 
 internal class Program
 {
     static void Main()
     {
-        string path = "C:\\Users\\adunderdale\\langtest.txt";
-        var lines = File.ReadAllLines(path);
-
-        foreach (var line in lines) 
-        {
-            Lexer.Tokens.Clear(); // remove
-
-            Lexer.Tokenize(line);
-            Console.WriteLine("\n");
-
-            foreach (var c in Lexer.Tokens) 
-            {
-                Console.Write(c.Value);
-            }
-        }   
-
+        
     }
 }
