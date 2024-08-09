f5a4e027da350f89361342da4ce6227a4f2ed737
rxgq <ashtondunderdale@gmail.com>
2024-08-09 13:02:27 +0100

    change

--- src/Program.cs
+++ src/Program.cs
@@ -12,12 +12,13 @@ internal abstract class Program
             return;
         }
 
+
         var source = File.ReadAllLines(path);
 
         var lexer = new Lexer(source);
         var tokens = lexer.Tokenize();
 
-        lexer.Print();
+        //lexer.Print();
 
         var program = new Interpreter(tokens);
         program.Interpret();
